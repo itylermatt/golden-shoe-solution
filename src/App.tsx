36b154bfed3b34eb41a65ea7a4a96eadd9d855a5
itylermatt <matthewstylerian@gmail.com>
2021-02-18 14:29:00 +0200

    Added Tabs and an Alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import SnackBar from './components/SnackBar/SnackBar';
+import Tabs from './components/Tabs/Tabs';
 
 function App() {
+    const [tabs, setTabs] = useState(['men', 'women', 'kids']);
+
   return (
     <div className="App">
       <NavBar />
+      <Tabs tabs={tabs} />
+      <SnackBar />
     </div>
   );
 }
